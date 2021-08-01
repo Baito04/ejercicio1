1cb7643e5d8f3128adda115960bd47f9e9680e4f
Baito04 <87608667+Baito04@users.noreply.github.com>
2021-08-01 09:12:00 -0500

    Update basico.py

--- DEBER BYRON/operaciones_numeros/basico.py
+++ DEBER BYRON/operaciones_numeros/basico.py
@@ -1,33 +1,33 @@
 class Basico:
-    def numeros_n(self, n=10):
-        print('Numeros del 1 al', n)
+    def num_n(self, n=10):
+        print('N del 1 al', n)
         for i in range(1, n+1):
             if i == n:
                 print(str(i) + '.')
             else: print(str(i) + ', ' ,end='')
 
-    def multiplo(self, numero=36):
-        print('Multiplos de', numero, '\n0, ', end='')
-        for i in range(1, numero+1):
-            if (numero * i) % i == 0:
+    def multiplo(self, num=36):
+        print('Multiplos de', num, '\n0, ', end='')
+        for i in range(1, num+1):
+            if (num * i) % i == 0:
                 if i == numero:
-                    print(str(i*numero) + '.')
-                else: print(str(i*numero) + ', ' ,end='')
+                    print(str(i*num) + '.')
+                else: print(str(i*num) + ', ' ,end='')
 
-    def divisores_numeros(self, numero=42):
-        print('Divisores de', numero)
-        for i in range(1, numero+1):
-            if numero % i == 0:
-                if i == numero:
+    def divisores_numeros(self, num=42):
+        print('Divisores de', num)
+        for i in range(1, num+1):
+            if num % i == 0:
+                if i == num:
                     print(str(i) + '.')
                 else: print(str(i) + ', ' ,end='')
 
-    def primo(self, numero=7):
+    def primo(self, num=7):
         primo = False
         print('Número primo')
-        if numero >= 2:
-            for i in range(2, numero):
-                if numero % i == 0:
+        if num >= 2:
+            for i in range(2, num):
+                if num % i == 0:
                     print(' -No es primo')
                     primo = True
                     break
@@ -36,13 +36,13 @@ class Basico:
         else:
             print(' -No es primo')
 
-    def perfecto(self, numero=6):
+    def perfecto(self, num=6):
         print('Número perfecto')
         sum = 0
-        for i in range(1, numero):
-            if numero % i == 0:
+        for i in range(1, num):
+            if num % i == 0:
                 sum += i
-        if numero == sum:
+        if num == sum:
             print(' -El número es perfecto')
         else:
             print(' -El número no es perfecto')
