3ad4973105499fe398f02cd235e09ad786a7804a
Baito04 <87608667+Baito04@users.noreply.github.com>
2021-08-01 09:06:20 -0500

    Update calculadoraEstandar.py

--- calculadoraEstandar.py
+++ calculadoraEstandar.py
@@ -2,15 +2,15 @@ from .calculadora import Calculadora
 
 
 class calEstandar(Calculadora):
-    def __init__(self, numero1=0, numero2=0):
-        super().__init__(numero1=numero1, numero2=numero2)
+    def __init__(self, n1=0, n2=0):
+        super().__init__(n1=n1, n2=n2)
 
     def multiplicacion(self):
-        mul = (self.numero1 * self.numero2)
+        mul = (self.n1 * self.n2)
         return round(mul, 2)
 
     def exponente(self):
-        expo = (self.numero1 ** self.numero2)
+        expo = (self.n1 ** self.n2)
         return round(expo, 2)
 
     def valorAbsoluto(self, numero):
